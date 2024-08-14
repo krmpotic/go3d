2fbd5294220e846d9c17ff8f70da4d39f5f67f7c
Luka Krmpotic <luka.krmpotic@dfgcon.si>
2024-08-14 11:38:55 +0200

    separate deg functions into own file

--- coordinate.go
+++ coordinate.go
@@ -8,20 +8,6 @@ import (
 
 const Tau = 2 * math.Pi
 
-type deg float64
-
-func (d deg) String() string {
-	return fmt.Sprintf("%+ 6.4f°", float64(d))
-}
-
-func radToDeg(r float64) deg {
-	return deg(r / Tau * 360)
-}
-
-func degToRad(d deg) float64 {
-	return float64(d) / 360 * Tau
-}
-
 type northeast struct {
 	north, east float64
 }
---
+++ deg.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+type deg float64
+
+func (d deg) String() string {
+	return fmt.Sprintf("%+ 6.4f°", float64(d))
+}
+
+func radToDeg(r float64) deg {
+	return deg(r / Tau * 360)
+}
+
+func degToRad(d deg) float64 {
+	return float64(d) / 360 * Tau
+}
