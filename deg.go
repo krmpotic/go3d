7adf69b8254eb5a8ce178d227fb19276df7e0c49
Luka Krmpotic <luka.krmpotic@dfgcon.si>
2024-08-14 14:27:43 +0200

    remove unused String()

--- deg.go
+++ deg.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"math"
 )
 
 type deg float64
-
-func (d deg) String() string {
-	return fmt.Sprintf("%+ 6.4f°", float64(d))
-}
 
 func radToDeg(r float64) deg {
 	return deg(r / 2 / math.Pi * 360)
